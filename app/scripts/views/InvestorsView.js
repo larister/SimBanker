a4b179c445795cabf1440a59e05667cec6bb3502
katie.clark <katie.clark@icrossing.co.uk>
2013-09-14 22:28:35 +0100

    THis is broken

---
+++ app/scripts/views/InvestorsView.js
@@ -0,0 +1,57 @@
+define([
+    'helpers/InvestorDemandHelper',
+    'mustache!investorsView',
+    'mustache!investor'
+], function(
+    InvestorDemandHelper,
+	investorsViewTemplate,
+    investorTemplate
+){
+    'use strict';
+
+    return Backbone.View.extend({
+
+        initialize: function(){
+            this.investorHelper = new InvestorDemandHelper();
+            this.listenTo(this.investorHelper, 'triggerVisit', _(this.investorsAppear).bind(this));           
+        },
+
+        render: function(){
+            this.$el.append(investorsViewTemplate());
+        },
+
+        investorsAppear: function(investorType){
+            var self = this;
+            var v = this.$('.inv-main');
+
+            var investor = investorTemplate({type: investorType, right: -100});
+
+            window.setTimeout(function() {
+                self.moveRight(investor);
+            }, 3000);
+
+            v.append(investor);
+            investor.animate({opacity: 1}, 100);
+            investor.moveLeft();
+        },
+
+        investorsDisappear: function() {
+            
+        },
+
+        moveLeft: function(investor){
+            investor.animate({opacity: 1}, 100);
+            investor.style.right = parseInt(investor.style.right) + 10 + 'px';
+            animate = setTimeout(moveLeft,20); // call moveLeft in 20msec
+        },
+
+        moveRight: function(investor){
+            investor.style.left = parseInt(investor.style.left) + 10 + 'px';
+            animate = setTimeout(moveRight,20); // call moveRight in 20msec
+            investor.animate({opacity: 0}, 100);
+        }
+
+    });
+});
+		
+		
