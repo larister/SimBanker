54658a9be9f5c7cde141a3a98eeed4e213112e7e
Alastair Lockie <alastair.lockie@gmail.com>
2013-09-14 16:10:41 +0100

    Added spawn helper, fleshed out base template

--- app/scripts/views/MortgageMarketView.js
+++ app/scripts/views/MortgageMarketView.js
@@ -1,11 +1,25 @@
-define(['mustache!house'], function(houseTemplate) {
+define([
+    'SpawnHelper',
+    'mustache!house'
+], function(
+    SpawnHelper,
+    houseTemplate
+){
     'use strict';
 
     return Backbone.View.extend({
 
+        initialize: function(){
+            this.listenTo(SpawnHelper, 'spawnMortgage', this.showHouse);
+        },
+
         render: function(){
             this.$el.append('Well hello sweetie pie!');
             this.$el.append(houseTemplate());
+        },
+
+        showHouse: function(houseType){
+            // Show data house!
         }
 
     });
