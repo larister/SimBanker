27564c434da2de044f458a026178199812f1ad6d
Alastair Lockie <alastair.lockie@gmail.com>
2013-09-15 16:55:43 +0100

    Finished default behaviour

--- app/scripts/collections/MortgageCollection.js
+++ app/scripts/collections/MortgageCollection.js
@@ -7,16 +7,22 @@ define([
 
     return Backbone.Collection.extend({
 
-        defaultChance: 0.05,
+        defaultChance: 0.5,
 
         model: MortgageModel,
 
         mortgageDefault: function(){
-            var randomIndex;
+            var randomIndex, randomMortgage;
+
+            if(this.isEmpty()){
+                return;
+            }
 
             if(Math.random() < this.defaultChance){
                 randomIndex = Math.floor(Math.random() * this.length);
-                this.remove(this.at(randomIndex));
+                randomMortgage = this.at(randomIndex);
+                this.trigger('defaulted', randomMortgage.toJSON());
+                this.remove(randomMortgage);
             }
         }
 
