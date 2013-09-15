3e1713381a5f886b6128db6c8d8088e037dff4ee
Alastair Lockie <alastair.lockie@gmail.com>
2013-09-15 12:40:05 +0100

    Cleaned up collections, added mortgageDefault

--- app/scripts/collections/CDOCollection.js
+++ app/scripts/collections/CDOCollection.js
@@ -1,22 +1,13 @@
 define([
     'models/CDOModel'
-    ], 
-    function(
-        CDOModel
-    ) {
+], function(
+    CDOModel
+) {
     'use strict';
 
     return Backbone.Collection.extend({
 
         model: CDOModel,
-
-        initialize: function(){
-            
-        },
-
-        render: function(){
-            
-        }
 
     });
 
--- app/scripts/collections/MortgageCollection.js
+++ app/scripts/collections/MortgageCollection.js
@@ -1,21 +1,23 @@
 define([
     'models/MortgageModel'
-    ], 
-    function(
-        MortgageModel
-    ) {
+], function(
+    MortgageModel
+){
     'use strict';
 
     return Backbone.Collection.extend({
 
+        defaultChance: 0.05,
+
         model: MortgageModel,
 
-        initialize: function(){
-            
-        },
+        mortgageDefault: function(){
+            var randomIndex;
 
-        render: function(){
-
+            if(Math.random() < this.defaultChance){
+                randomIndex = Math.floor(Math.random() * this.length);
+                this.remove(this.at(randomIndex));
+            }
         }
 
     });
